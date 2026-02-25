eafab17f0be0649497c805a7b0336ef9b22c38cb
Quvonch299 <muminovquvonchbek85@gmail.com>
2026-02-25 22:53:55 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Profile from "./pages/Profile";
+import Profile from "./pages/profile";
 
 export default function App() {
   return (

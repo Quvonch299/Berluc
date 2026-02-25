2cabad93ff8361b9b6f81be23a5f8be9a1403c35
Quvonch299 <muminovquvonchbek85@gmail.com>
2026-02-25 22:50:20 +0500

    as

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
-
-import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Login from "./pages/login";
-import Profile from "./Profile";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/" element={<div>Home Page</div>} />
+    </Routes>
   );
 }

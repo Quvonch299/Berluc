07a526ae8a9bbcb38131248119da7af814201720
Quvonch299 <muminovquvonchbek85@gmail.com>
2026-02-25 22:55:12 +0500

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Profile from "./pages/profile";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/profile" element={<Profile />} />
       <Route path="/" element={<div>Home Page</div>} />
     </Routes>
   );
